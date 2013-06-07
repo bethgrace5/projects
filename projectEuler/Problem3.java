61c6bb6867b2f0bb7bcb5fa1302d08d578afb3bb
Beth Armitage <bethgrace5@gmail.com>
2013-06-07 00:42:48 -0700

    Works much quicker, and is more accurate

--- projectEuler/Problem3.java
+++ projectEuler/Problem3.java
@@ -14,30 +14,16 @@ import java.util.Set;
 import java.util.Scanner;
 public class Problem3{
 		private Scanner in1 = new Scanner("C:/Users/Bethgrace55/Documents/primesList.txt");
-/*
- * FIXME: my methods don't like to access a private set, nor will main() reference private variables
- * therefore, I have it set as static.
- */
-		static Set<Long> primesFound = new HashSet<Long>(500);{
-			Long num = in1.nextLong();
-/*FIXME: (line 23-25) initializing HashSet primesFound with list of primes in the File is not working.	
- * I want to move the declaration of PrimesFound to main() so it's guaranteed to run, but
- * then I'm worried about methods belonging to the class being able to access the HashSet.  
- */
-			for (int a=0; a<100; a++){
-				primesFound.add(num);
-			}
-			in1.close();
-		}
+		private static Set<Long> primesFound = new HashSet<Long>(500);
 	/**
 	 * generates a list of factors and uses isPrime() to return only the prime factors.
 	 * @param number the number to generate factors from.
 	 * @return returnList an array of factors that are prime.
 	 */
 	public static Long[] primeFactorsOf( Long number){
-		Long [] returnList = new Long [1000];
+		Long [] returnList = new Long [100];
 		int counter = 0;
-		for (Long i= 2L; i<(number); i++){
+		for (Long i= 3L; i<=(number/2); i+=2){
 			 Long temp = number%i;
 			 if (temp==0){
 				 if( primesFound.contains(i) || isPrime(i)){
@@ -56,37 +42,40 @@ public class Problem3{
 	 */
 	public static boolean isPrime(Long temporary){
 		boolean primeness = true;
-		if(primesFound.contains(temporary)){
-				return true;
-			}
-		for (Long i= 2L; i<(temporary) && primeness ; i++){
-			if (temporary%i==0){
+		if(temporary%2==0){
+			return false;
+		}
+		for (Long i= 3L; i<(temporary/2) ; i+=2){
+			if (primeness && temporary%i==0){
 				primeness = false;
 			}
-			if (i==(temporary-1)&& primeness ){
-				primesFound.add(temporary);
-				primeness=true;
+			if (primeness){
+				primesFound.add(i);
 			}
 	}
 	return primeness;
 	}
 	
-	public static void main(String[] args){
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args)throws FileNotFoundException {
+		File primesList = new File ("C:/Users/Bethgrace55/Documents/primesList.txt");
+		Scanner fileReader = new Scanner(primesList);
+		for (int i=0; i<5098; i++){
+			primesFound.add(fileReader.nextLong());
+		}
 		Long[] start = new Long [100];
+		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a number to find prime factors of.");
 		start = primeFactorsOf(in.nextLong());
 		in.close();
-		System.out.print(primesFound.toString());
+		
+		
 		for (int i=0; i<100; i++){
+			System.out.println(start[i]);
 			if(start[i+1]==null){
-				System.out.println(start[i]);
 				break;
 			}
-		if(isPrime(start[i]))
-			System.out.println(start[i]);
-		else continue;
 		}
+		
 	}
 }
 
