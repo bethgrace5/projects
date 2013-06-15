6011d54016ef643f6327b96939184330d36f30d2
Beth Armitage <bethgrace5@gmail.com>
2013-06-14 21:14:18 -0700

    reads from one file and writes to another uses scanner instead of iterator

--- random/IteratorPractice.java
+++ random/IteratorPractice.java
@@ -1,19 +1,23 @@
 package random;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.TreeSet;
 import java.util.Iterator;
-
-
+import java.util.Scanner;
+import java.io.PrintWriter;
 public class IteratorPractice {
-	public static void main(String[] args){
+	public static void main(String[] args) throws FileNotFoundException{
 		TreeSet<Integer> numbers = new TreeSet<Integer>();	
 		Iterator<Integer> printNumbers = numbers.iterator();
-		numbers.add(3);
-		numbers.add(2);
-		numbers.add(4);
-		numbers.add(3);
-	
-		System.out.println(numbers.toString());
-	
+		File printNumbersTo= new File(System.getProperty("user.home")+"\\Documents\\someNumbers.txt");
+		Scanner read = new Scanner (new File( System.getProperty("user.home")+"\\Documents\\number.txt"));
+		PrintWriter pw = new PrintWriter(printNumbersTo);
+		while( read.hasNextInt()){
+			numbers.add(read.nextInt());
+			}
+		pw.print(numbers.toString());
+		pw.close();
+		read.close();
 	}
 	
 }	
