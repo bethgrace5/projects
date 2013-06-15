f9df8db998c607a8028e10a54ac3c3b31e85bb6a
Beth Armitage <bethgrace5@gmail.com>
2013-06-14 18:01:48 -0700

    treeset replaces hashset, printwriter set up, but iterator not working

--- projectEuler/Problem3.java
+++ projectEuler/Problem3.java
@@ -1,12 +1,17 @@
 package projectEuler;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Properties;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 /**
  * @author Bethgrace5
  * The prime factors of 13195 are 5, 7, 13 and 29.
@@ -14,7 +19,7 @@ import java.util.Set;
  */
 import java.util.Scanner;
 public class Problem3{
-	public static Set<Long> primesFound = new HashSet<Long>(100);
+	public static TreeSet<Long> primesFound = new TreeSet<Long>();
 	public static Iterator<Long> checkForFactors = primesFound.iterator();
 	/**
 	 * @param number - the number to generate primes factors of.
@@ -22,19 +27,18 @@ public class Problem3{
 	 */
 	public static ArrayList<Long> primeFactorsof(Long number){
 		ArrayList<Long> returnList = new ArrayList<Long>();
-		if (Collections.max(primesFound) < number/2){
-			for(Long i=Collections.max(primesFound)+1; i<= number/2; i++){
+		if (primesFound.last() < number/2){
+			for(Long i=primesFound.last()+1; i<= number/2; i++){
 				if (isPrime(i)){
 					primesFound.add(i);
-	/*
-	 *TODO: use a print writer to add new primes to primesArchive File.
-	 */
 				}
 			}
 		}
+		//FIXME: Iterator not iterating, or else returnList would be adding factors from primesFound. 
 		while (checkForFactors.hasNext()){
-			if (number%checkForFactors.next()==0){
-				returnList.add(checkForFactors.next());
+			long next = checkForFactors.next();
+			if (number%next==0){
+				returnList.add(next);
 		}
 		}
 		return returnList;
@@ -57,20 +61,26 @@ public class Problem3{
 	return primeness;
 	}
 	
-	public static void main(String[] args)throws FileNotFoundException {
+	public static void main(String[] args)throws IOException {
 		String home = System.getProperty("user.home");
-		Scanner numFromFile = new Scanner("C:/Users/" + home + "/Documents/primesList.txt");
+		File dir =  new File(home + "\\Documents\\primesList.txt");
+		Scanner numFromFile = new Scanner(dir);
 		//loading primesFound with up to date list of primes.
 		while(numFromFile.hasNextLong()){
-			primesFound.add(numFromFile.nextLong());
+		primesFound.add(numFromFile.nextLong());
 		}
 		
 		ArrayList<Long> start = new ArrayList<Long>();
-		
+		//proof that file is being read. 
+		System.out.print(primesFound.toString());
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a number to find prime factors of.");
 		
 		start = primeFactorsof(in.nextLong());
+		FileWriter fw = new FileWriter(dir, true);
+		PrintWriter out = new PrintWriter(fw);
+		
+		out.close();
 		in.close();
 		numFromFile.close();
 			System.out.println(start.toString());
