56edcfbc8b65e3004582d39b7e959b13b9f3bfcd
Beth Armitage <bethgrace5@gmail.com>
2013-06-18 12:41:51 -0700

    works. created iterator after data was added

--- random/IteratorPractice.java
+++ random/IteratorPractice.java
@@ -1,23 +1,18 @@
 package random;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.TreeSet;
 import java.util.Iterator;
-import java.util.Scanner;
-import java.io.PrintWriter;
 public class IteratorPractice {
-	public static void main(String[] args) throws FileNotFoundException{
+	public static void main(String[] args){
 		TreeSet<Integer> numbers = new TreeSet<Integer>();	
+		numbers.add(2);
+		numbers.add(9);
+		numbers.add(7);
+		numbers.add(3);
+		numbers.add(2);
 		Iterator<Integer> printNumbers = numbers.iterator();
-		File printNumbersTo= new File(System.getProperty("user.home")+"\\Documents\\someNumbers.txt");
-		Scanner read = new Scanner (new File( System.getProperty("user.home")+"\\Documents\\number.txt"));
-		PrintWriter pw = new PrintWriter(printNumbersTo);
-		while( read.hasNextInt()){
-			numbers.add(read.nextInt());
-			}
-		pw.print(numbers.toString());
-		pw.close();
-		read.close();
+		while (printNumbers.hasNext()){
+			System.out.print(printNumbers.next()+", ");
+		}
 	}
 	
 }	
