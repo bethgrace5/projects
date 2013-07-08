7a33c8082b4345c2ce89c0177a310c3e395170d0
bethgrace5 <bethgrace5@gmail.com>
2013-07-07 23:01:44 -0700

    reads from file, prints to console, prints memory location, not numbers

--- Cprojects/someCStuff.c
+++ Cprojects/someCStuff.c
@@ -3,26 +3,21 @@
 #include<stdlib.h>
 
 int main(int argc, char *argv[]){
-	FILE *letters, *numbers, *concatenation;
+	FILE *numbers;
 	/* contents of letters:(on seperate lines)
 		A B C D E F G*/ 
 	/* contents of numbers:(on seperate lines)
 		1 2 3 4 5 6 7*/ 
-//open files
-	letters = fopen("letters.txt", "r");	
+//open file
 	numbers = fopen("numbers.txt", "r");	
-	concatenation = fopen("concatenation.txt", "w");	
 //create a place to store what is read from the files
-char nextletter[7];
-int nextnumber;
+int nextnumber[7];
 //print the files.
-while(fscanf(*letters, "%d", *nextletter)==1){
-	printf(*letters, "%s\n", *nextletter);
-}
-while(fscanf(*numbers, "%d", *nextnumber)==1){
-	printf(*numbers, "%d\n", *nextnumber);
-}
-//will create a concatenation of the two files in a new file.
-//after I read the files successfully...
+fread(nextnumber, sizeof(int), 7, numbers);
+int i=0;
+for(i=0; i<8; i++){
+printf("%d\n",nextnumber[i]);
+};
+fclose(numbers);
 return 0;
 }
