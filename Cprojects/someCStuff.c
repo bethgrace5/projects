5f2adc7f98b5a311cf5905957dc5d7d5b623f9a4
bethgrace5 <bethgrace5@gmail.com>
2013-07-07 23:09:22 -0700

    added second file back, still prints what I think is the memory location

--- Cprojects/someCStuff.c
+++ Cprojects/someCStuff.c
@@ -3,21 +3,27 @@
 #include<stdlib.h>
 
 int main(int argc, char *argv[]){
-	FILE *numbers;
+	FILE *numbers, *letters;
 	/* contents of letters:(on seperate lines)
 		A B C D E F G*/ 
 	/* contents of numbers:(on seperate lines)
 		1 2 3 4 5 6 7*/ 
 //open file
 	numbers = fopen("numbers.txt", "r");	
+	letters = fopen("letters.txt", "r");	
 //create a place to store what is read from the files
 int nextnumber[7];
-//print the files.
+int nextletter[7];
+//read the files.
 fread(nextnumber, sizeof(int), 7, numbers);
+fread(nextletter, sizeof(int), 7, letters);
+//print the files;
 int i=0;
 for(i=0; i<8; i++){
-printf("%d\n",nextnumber[i]);
+printf("number%d:%d\n",i, nextnumber[i]);
+printf("letter%d:%d\n",i, nextletter[i]);
 };
 fclose(numbers);
+fclose(letters);
 return 0;
 }
