720304577b05e41fcf5b603f3acb58c3775a98d1
bethgrace5 <bethgrace5@gmail.com>
2013-07-08 19:48:26 -0700

    prints everything in seconds without testing words

--- random/readWebPage.java
+++ random/readWebPage.java
@@ -14,10 +14,10 @@ Scanner in = new Scanner(pageLocation.openStream());
 	String word = in.next();
 	boolean found  = false;
 	while(in.hasNextLine() && !found){
-	if(word.substring(0,2).equals("<a")){
-	System.out.print(word);
-	found=true;
-	}
+	//if(word.substring(0,2)=="<a"){
+	System.out.print(in.nextLine());
+	//found=true;
+	//}
 }
 }
 
