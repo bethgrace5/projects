a845d8d578ea388336b87dc212e18eb266e1a751
bethgrace5 <bethgrace5@gmail.com>
2013-07-08 19:31:17 -0700

    reads web page, should search for the first link and print it or the contents around it and exit, as for now it takes too long to run for some reason

---
+++ random/readWebPage.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.net.*;
+import java.io.IOException;
+
+public class readWebPage{
+
+public static void main(String[] args)throws IOException{
+
+String address = "http://www.google.com";
+
+URL pageLocation = new URL(address);
+
+Scanner in = new Scanner(pageLocation.openStream());
+	String word = in.next();
+	boolean found  = false;
+	while(in.hasNextLine() && !found){
+	if(word.substring(0,2).equals("<a")){
+	System.out.print(word);
+	found=true;
+	}
+}
+}
+
+}
