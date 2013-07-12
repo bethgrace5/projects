c1bd3c4cb05f0c4ab4c6f8766875092a632b09df
Beth Armitage <bethgrace5@gmail.com>
2013-07-11 22:53:19 -0700

    was working really well, But quit scanning the file, I don't know why

--- projectEuler/Problem3.java
+++ projectEuler/Problem3.java
@@ -19,26 +19,33 @@ import java.util.TreeSet;
  */
 import java.util.Scanner;
 public class Problem3{
+	public static ArrayList<Long> append = new ArrayList<Long>();
 	public static TreeSet<Long> primesFound = new TreeSet<Long>();
-	public static Iterator<Long> checkForFactors = primesFound.iterator();
+	public static Iterator<Long> checkForFactors1, checkForFactors2; 
+	static Long end;
 	/**
 	 * @param number - the number to generate primes factors of.
 	 * @return returnList - the list of number's prime factors.
 	 */
 	public static ArrayList<Long> primeFactorsof(Long number){
 		ArrayList<Long> returnList = new ArrayList<Long>();
-		if (primesFound.last() < number/2){
-			for(Long i=primesFound.last()+1; i<= number/2; i++){
+		checkForFactors1 = primesFound.iterator();
+		end =primesFound.last();
+		if (end < number/2){
+			for(Long i=end; i<= number/2; i++){
 				if (isPrime(i)){
-					primesFound.add(i);
+					append.add(i);
 				}
 			}
 		}
-		//FIXME: Iterator not iterating, or else returnList would be adding factors from primesFound. 
-		while (checkForFactors.hasNext()){
-			long next = checkForFactors.next();
-			if (number%next==0){
+		checkForFactors2 = primesFound.iterator(); 
+		while (checkForFactors2.hasNext()){
+			long next = checkForFactors2.next();
+			
+			if (next<number/2 && number%next==0){
 				returnList.add(next);
+				System.out.println(next);
+			
 		}
 		}
 		return returnList;
@@ -60,7 +67,7 @@ public class Problem3{
 	}
 	return primeness;
 	}
-	
+
 	public static void main(String[] args)throws IOException {
 		String home = System.getProperty("user.home");
 		File dir =  new File(home + "\\Documents\\primesList.txt");
@@ -68,22 +75,25 @@ public class Problem3{
 		//loading primesFound with up to date list of primes.
 		while(numFromFile.hasNextLong()){
 		primesFound.add(numFromFile.nextLong());
+		System.out.println("added number");
 		}
-		
 		ArrayList<Long> start = new ArrayList<Long>();
-		//proof that file is being read. 
+		//proof that file is being read and primnesFound is initialized with file contents. 
 		System.out.print(primesFound.toString());
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a number to find prime factors of.");
-		
 		start = primeFactorsof(in.nextLong());
-		FileWriter fw = new FileWriter(dir, true);
-		PrintWriter out = new PrintWriter(fw);
-		
+		FileWriter out = new FileWriter(dir, true);
+		System.out.println(start.toString());
+		//updating file with newly accumulated data for future use.
+		for(int t=0; t<append.size(); t++){
+			out.write(append.get(t)+" ");
+			System.out.println(append.get(t)+"\n");
+		}
 		out.close();
 		in.close();
 		numFromFile.close();
-			System.out.println(start.toString());
+		
 	}
 }
 
