704213c9c9c7546431f39f9691e7f3d9316f2305
bethgrace5 <bethgrace5@gmail.com>
2013-07-20 11:10:42 -0700

    deleted methods, compiles now, prints some memory locations instead of contents though

--- reddit/mergeSort.c
+++ reddit/mergeSort.c
@@ -3,73 +3,52 @@
 //same as easy125(java) but written in C.
 
 //pre-declaration
-int *sort(int *toSort);
-int *merge(int *one, int *two);
 
 int main(int argc, char *argv[]){
 
 int unsorted []={692, 1, 32};
 int sorted []={0, 0, 0, 14, 15, 32, 123, 2431};
-int i;
+int i, e, temp, position;
 int len_unsorted = sizeof(unsorted)/sizeof(int);
 int len_sorted = sizeof(sorted)/sizeof(int);
 int mergedList [len_unsorted + len_sorted];
-int (*unst)[len_unsorted];
-int (*st) [len_sorted];
+int returnarray [len_unsorted];
+int (*unst)[len_unsorted] = &unsorted;
+int (*st) [len_sorted] = &sorted;
 int (*mrgd)[sizeof(mergedList)];
+int (*ret_array)[len_unsorted]= &returnarray;
 
-printf("before sorting: \n");
+	printf("before sorting: \n");
 for(i=0;i<len_unsorted; i++){
-	printf("%d, ",unsorted[i]);
+	printf("%d, \n",*unst[i]);
 }
-// "error: lvalue required as unary '&' operand"
-unst = &(sort(*unst));
-
-printf("after sorting: \n");
-for(i=0;i<sizeof(unsorted)/sizeof(int); i++){
-	printf("%d, ",unsorted[i]);
+//sorting unsorted here
+for(i=0; i<len_unsorted; i++){
+temp = unsorted[i];
+position = i;
+	if(temp < unsorted[i]){
+	temp = unsorted[i];
+	}
+	*ret_array[len_unsorted-i-1] = temp;
+	unsorted[position]=0;
 }
 
-// "error: lvalue required as unary '&' operand"
-mrgd = &(merge(*unst, *st));
+	printf("after sorting: \n");
+for(e=0;e<(len_unsorted); e++){
+	printf("%d, \n",*ret_array[e]);
+}
+//will merge the two lists here.
+
+
+
 printf("after merging: \n");
-for(i=0;i<sizeof(unsorted)/sizeof(int); i++){
-	printf("%d, ",unsorted[i]);
+for(i=0;i<len_unsorted+len_sorted; i++){
+	printf("%d, ",*unst[i]);
 }
 
 
-
-
-//testing values for length of unsorted and sorted.
 printf("Unsorted contains %d elements\n",len_unsorted);
 printf("Sorted contains %d elements\n",len_sorted);
 
-//I realize just how much Java spoiled me with ArrayLists...
-//this function should return a pointer
-int *sort(int *toSort){
-int (*ret_array)[len_unsorted];
-int e, temp, position;
-for(i=0; i<len_unsorted; i++){
-temp = toSort[i];
-position = i;
-	for(e=0; e<len_unsorted; e++){
-	if(temp < toSort[e]){
-	temp = toSort[e];
-	position = e;	
-	--i;
-	}
-	temp = *ret_array[len_unsorted-i-1];
-	toSort[position]=0;
-	return *ret_array;
-}
-}
-	*ret_array[i]=temp;
-	
 
-return toSort;
-}
-int *merge(int *one, int *two){
-//TODO:merge two sorted lists here.
-
-}
 }
