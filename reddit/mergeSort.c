424f3140b2d8a9d233f1c15a736c24d220b8fea3
bethgrace5 <bethgrace5@gmail.com>
2013-07-20 23:12:59 -0700

    fixed address printing by using actual array instead of pointer, sorting almost working

--- reddit/mergeSort.c
+++ reddit/mergeSort.c
@@ -19,23 +19,33 @@ int (*mrgd)[sizeof(mergedList)];
 int (*ret_array)[len_unsorted]= &returnarray;
 
 	printf("before sorting: \n");
-for(i=0;i<len_unsorted; i++){
-	printf("%d, \n",*unst[i]);
+for(i=0;i<(len_unsorted); i++){
+	printf("%d, \n",unsorted[i]);
 }
-//sorting unsorted here
-for(i=0; i<len_unsorted; i++){
+//Here's my sorting idea.
+//Find largest in unsorted. Set bottom of return array to value 
+//Change that value to 0 in unsorted. Repeat.
+for(i=0; i<(len_unsorted); i++){
 temp = unsorted[i];
 position = i;
-	if(temp < unsorted[i]){
-	temp = unsorted[i];
+for(e=0; e<len_unsorted; e++){
+	if(temp < unsorted[e]){
+	temp = unsorted[e];
+	position = e;
+	
 	}
-	*ret_array[len_unsorted-i-1] = temp;
+//trying to put the last nonzero element (smallest number)into returnarray[0];
+	if((i== len_unsorted-1) && (unsorted[e]< temp)){
+	returnarray[0] = unsorted[e];
+	}
+	returnarray[len_unsorted-i-1] = temp;
 	unsorted[position]=0;
+}
 }
 
 	printf("after sorting: \n");
 for(e=0;e<(len_unsorted); e++){
-	printf("%d, \n",*ret_array[e]);
+	printf("%d, \n",returnarray[e]);
 }
 //will merge the two lists here.
 
