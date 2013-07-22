ab12d9b2af9035b5e567f800e932421089864483
Beth Armitage <bethgrace5@gmail.com>
2013-07-22 14:23:00 -0700

    creating a seperate space for implementing visual interfaces

---
+++ Scheduler/Visual.java
@@ -0,0 +1,16 @@
+package Scheduler;
+import javax.swing.BoxLayout;
+import java.awt.Container;
+import java.awt.Component;
+import javax.swing.JFrame;
+
+// A seperate space for visual stuff.
+// with help from: http://docs.oracle.com/javase/tutorial/uiswing/components/frame.html
+
+public class Visual {
+
+	
+	JFrame frame = new JFrame("demo frame");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+}
