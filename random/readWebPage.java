19e07b396e33781f89d1cb1dcfa8e411f8a27d67
Beth Armitage <bethgrace5@gmail.com>
2013-09-09 23:45:06 -0700

    looking into GSON

--- random/readWebPage.java
+++ random/readWebPage.java
@@ -1,24 +1,21 @@
+package random;
+
 import java.util.Scanner;
 import java.net.*;
 import java.io.IOException;
 
 public class readWebPage{
 
-public static void main(String[] args)throws IOException{
-
-String address = "http://www.google.com";
+public static void main(String[] args) throws IOException{
+String address = "http://api.wunderground.com/api/c6c6d53295b46eb6/conditions/q/CA/San_Francisco.json";
 
 URL pageLocation = new URL(address);
-
+//TODO: look into JSON, and how to interpret it via GSON (https://code.google.com/p/google-gson/)
 Scanner in = new Scanner(pageLocation.openStream());
-	String word = in.next();
-	boolean found  = false;
-	while(in.hasNextLine() && !found){
-	//if(word.substring(0,2)=="<a"){
-	System.out.print(in.nextLine());
-	//found=true;
-	//}
-}
-}
+	while(in.hasNextLine()){
+	System.out.println(in.nextLine());
+		}
+	in.close();
+	}
 
 }
