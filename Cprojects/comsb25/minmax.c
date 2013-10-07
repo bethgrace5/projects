205b21e6b59af3e8ed7ecfc7e143d63ac328b3c7
Beth Armitage <bethgrace5@gmail.com>
2013-10-07 00:50:40 -0700

    finished, must research pointers to pointers more

--- Cprojects/comsb25/minmax.c
+++ Cprojects/comsb25/minmax.c
@@ -15,14 +15,24 @@
  * maximum values in an array of doubles with size elements.
  *
  * @param array array of doubles
- * @param size number of elements in array
- * @param min pointer to a pointer which should point to the minimum value
- *          in the array upon function return 
- * @param max pointer to a pointer which should point to the maximum value
- *          in the array upon function return 
+ * @param size number of elements
+ * @param min pointer to a pointer to the minimum value in the array
+ * @param max pointer to a pointer to the maximum value in the array 
  */
-void minmax(double array[], const int size, double **min, double ** max) {
-
+void minmax(double array[], const int size, double **min, double **max) {
+	int i;
+	*min = &array[0];
+	*max = &array[0];
+	
+	for(i=0; i<size; i++){
+		if(array[i] < **min){
+			*min = &array[i];
+		}
+		if(array[i] > **max){
+			*max = &array[i];
+		}
+	}
+	return;
 }
 
 /*
@@ -32,14 +42,19 @@ void minmax(double array[], const int size, double **min, double ** max) {
  * @param size number of elements in array
  */
 void readDoubles(double *array, int size) {
-	// Replace this comment with your code 
+	while(size > 0){
+		scanf("%lf", array);
+		array ++;
+		size--;
+	}
+	return;
 }
 
 int main() {
 	const int size = 100;
 	double *min, *max;
 	double array[size];
-
+	
 	readDoubles(array, size);
 	minmax(array, size, &min, &max);
 	printf("Minimum value = %f, Maximum = %f\n", *min, *max);
