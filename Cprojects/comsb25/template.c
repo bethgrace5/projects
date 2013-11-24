a8c30bb84e2001b549f7f65ddb29e032c41e3ec9
Beth Armitage <bethgrace5@gmail.com>
2013-11-23 19:15:02 -0800

    my template for Coms25 assignments

---
+++ Cprojects/comsb25/template.c
@@ -0,0 +1,17 @@
+/**
+* This program 
+*
+* @author Bethany Armitage
+* Course: Comsb25
+* Created: 
+* Source File: 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+return 0;
+}
