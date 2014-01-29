767ff81eecd8b029df2ecd3fc8bedf4d796b0f26
Beth Armitage <bethgrace5@gmail.com>
2014-01-29 15:02:07 -0800

    This was all given to us

---
+++ Cprojects/comsb35/Lexicon_Test.cpp
@@ -0,0 +1,24 @@
+/*
+* A program that exercises the Lexicon class.
+*
+* @author      Hal Mendoza
+* Course:      COMS B35
+* Created:     Jan 25, 2013
+* Source File: Lexicon._test.cpp
+*/
+
+#include "Lexicon.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	Lexicon lexicon("Enhanced North American Benchmark Lexicon.txt");
+	string word;
+
+	cout << "Enter words to be checked. Enter EOF (Ctrl-z on Windows) when you are done\n";
+	cin >> word;
+	while (!cin.eof()) {
+		cout << word << " is" << (lexicon.isWord(word) ? "":" NOT") << " a word\n";
+		cin >> word;
+	}
+}
