8ed710d271db4e7591fb5867f6ac4c04351cb18e
Beth Armitage <bethgrace5@gmail.com>
2014-02-19 22:07:00 -0800

    set up basics

---
+++ classwork/comsb35/lab05/main.cpp
@@ -0,0 +1,33 @@
+
+/**
+ * This program exercises the orderedList class
+ * Source File:	main.cpp
+ */
+
+#include <iostream>
+#include "orderedList.h"
+using namespace std;
+
+int main() {
+    orderedList<string> stringList;
+    orderedList<int> intList;
+
+    intList.insert(8);
+    intList.insert(1);
+    intList.insert(5);
+    intList.insert(2);
+    //cout << intList.find(4) << endl;
+    //cout << intList.find(5) << endl;
+    cout << intList.toString() << endl;
+
+    stringList.insert("great");
+    stringList.insert("Penguin");
+    stringList.insert("banana");
+    stringList.insert("jungle");
+    //stringList.find("apple") << endl;
+    //stringList.find("banana") << endl;
+    cout << stringList.toString() << endl;
+
+
+}
+
---
+++ classwork/comsb35/lab05/orderedList.h
@@ -0,0 +1,80 @@
+
+/**
+ * orderedList is a container class that can hold a list of any
+ * type for which the insertion operator (<<) and the less than
+ * operator (<) is defined.
+ *
+ * @author 		Bethany Armitage
+ * Course: 		COMS B35
+ * Created: 	Feb 18, 2013
+ * Source File: orderedList.h
+ */
+
+#ifndef ORDEREDLIST_H_
+#define ORDEREDLIST_H_
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+template <class T>
+class orderedList {
+public:
+    void insert(const T& item);
+    bool find(const T& item) const;
+    string toString() const;
+private:
+    //friend ostream& operator 
+    std::vector<T> container;
+    //TODO: helper function to order list? mergesort?
+};
+
+#endif /* ORDEREDLIST_H */
+
+/**
+* inserts item in list in order
+* (alphabetical or numerical depending on type)
+* @param T& item the item to insert
+* 
+*/
+template <class T>
+void orderedList<T>::insert(const T& item){
+    //container.insert(item);
+    return;
+
+}
+
+/**
+* returns true if item exists in list
+* @param T& item
+* @return true if found, false if not found
+*/
+template <class T> 
+bool orderedList<T>::find(const T& item) const{
+    return std::find(container.begin(), container.end());
+}
+
+/*
+* puts all of the items in a string with newlines in between each
+* @return string of items
+*
+*/
+template <class T> 
+string orderedList<T>::toString() const{
+    int stop = container.end();
+    string tmp;
+    for(int i=0; i< stop; i++){
+        //concatenate tmp
+        tmp += container[i] + "\n";
+    }
+    //suggested to use ostringstream or stringstream object to
+    //accumulate objects in the list
+    return tmp;
+
+}
+
+
+
