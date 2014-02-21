65722ad1573aab3b6edcc515dea987b9a49af00b
Beth Armitage <bethgrace5@gmail.com>
2014-02-20 21:14:10 -0800

    templates: finished except helper to arrange

--- classwork/comsb35/lab05/orderedList.h
+++ classwork/comsb35/lab05/orderedList.h
@@ -15,7 +15,8 @@
 
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <sstream>
+#include <iostream>
 
 using namespace std;
 
@@ -26,10 +27,9 @@ public:
     void insert(const T& item);
     bool find(const T& item) const;
     string toString() const;
+    void putInOrder();
 private:
-    //friend ostream& operator 
     std::vector<T> container;
-    //TODO: helper function to order list? mergesort?
 };
 
 #endif /* ORDEREDLIST_H */
@@ -38,13 +38,11 @@ private:
 * inserts item in list in order
 * (alphabetical or numerical depending on type)
 * @param T& item the item to insert
-* 
 */
 template <class T>
 void orderedList<T>::insert(const T& item){
-    //container.insert(item);
+    container.push_back(item);
     return;
-
 }
 
 /**
@@ -54,25 +52,42 @@ void orderedList<T>::insert(const T& item){
 */
 template <class T> 
 bool orderedList<T>::find(const T& item) const{
-    return std::find(container.begin(), container.end());
+    int stop = container.size();
+    for(int i=0; i<stop; i++){
+        if( ((T) container.at(i)) == item)
+            return true;
+        }
+    return false;
 }
 
 /*
 * puts all of the items in a string with newlines in between each
 * @return string of items
-*
 */
 template <class T> 
 string orderedList<T>::toString() const{
+
+    //putInOrder();
+
+    ostringstream os;
     int stop = container.size();
-    string tmp;
+    string tmp= "";
     for(int i=0; i< stop; i++){
-        //concatenate tmp
-        tmp += container[i] + "\n";
+        // change any type read to string
+        os << container[i];
+
+        //add string to accumulation
+        tmp = tmp + os.str() + "\n";
+
+        // reset string buffer
+        os.str("");
     }
-    //suggested to use ostringstream or stringstream object to
-    //accumulate objects in the list
     return tmp;
+
+}
+void putInOrder(){
+
+// put vector in numerical or alphabetical order
 
 }
 
